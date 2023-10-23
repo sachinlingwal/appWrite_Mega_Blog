647bd58d9ec8d02a78e91d1569fec76ea7078613
sachinlingwal <sachinlingwal48@gmail.com>
2023-10-23 10:23:34 +0530

    Appwrite database, file upload and custom queries

---
+++ src/appwrite/ConfigService.js
@@ -0,0 +1,116 @@
+import { Client, Databases } from "appwrite";
+import configEnv from "../config/ConfigEnv";
+
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+
+  constructor() {
+    this.client
+      .setEndpoint(configEnv.appWriteUrl)
+      .setProject(configEnv.appWriteProjectId);
+
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        configEnv.appWriteDatabaseId,
+        configEnv.appWriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        configEnv.appWriteDatabaseId,
+        configEnv.appWriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async deletePost(slug) {
+    try {
+      return await this.databases.deleteDocument(
+        configEnv.appWriteDatabaseId,
+        configEnv.appWriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        configEnv.appWriteDatabaseId,
+        configEnv.appWriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.getDocument(
+        configEnv.appWriteDatabaseId,
+        configEnv.appWriteCollectionId,
+        queries
+      );
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(configEnv.appWriteBucketId), file;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  async deleteFile(fileId) {
+    try {
+      return await this.bucket.deleteFile(configEnv.appWriteBucketId), fileId;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  getFilePreview(fileId) {
+    return this.bucket.getFilePreview(configEnv.appWriteBucketId, fileId);
+  }
+}
+
+const service = new Service();
+
+export default service;
