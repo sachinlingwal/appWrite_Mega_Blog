e1d9a7c140593884596e050a24c89e40fdbbd2b6
sachinlingwal <sachinlingwal48@gmail.com>
2023-10-23 21:48:05 +0530

    React Hook Form

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
+import Button from "./Button";
 import Logo from "./Logo";
 import Container from "./container/Container";
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
 import LogoutBtn from "./header/LogoutBtn";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, Button };
